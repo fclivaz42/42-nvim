f83ef6184a1330546fd7d54ff643a7e902dac51d
fclivaz42 <fclivaz@pm.me>
2024-06-05 05:33:21 +0200

    just a few changes

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
---[[ broller config :) ]] --
+--[[ 42-Nvim config file ]] --
+--
+-- 42-Nvim is a neovim configuration, supercharged for 42 Students.
 
 -- Set Vim settings. Necessary before lazy is run.
 require "config.vim_settings"
@@ -28,16 +30,18 @@ end
 -- Load Lazy and make sure every plugin is installed.
 require "lazy_load"
 
--- Load config/init.lua
+-- Load config/init.lua which will load every plugin configuration.
 require "config"
 
 -- Launch! :)
 if (vim.g.user42 == nil) then
 	vim.g.user42 = vim.env.USER
 end
-vim.notify = require("notify")
-vim.notify("Welcome back " .. vim.g.user42 .. "! :)\nUse 'space-T' to switch between themes.", "info", { title = " 42-Nvim" })
 
-if (vim.g.user42 == "SET YOUR USER UP") then
-vim.notify("If you see this you havent configured your stuff!\nDon't forget to take a peek to your ~/.config/nvim", "error", { title = " 42-Nvim" })
+vim.notify = require("notify")
+
+if (vim.g.user42 ~= "SET YOUR USER UP") then
+	vim.notify("Welcome back " .. vim.g.user42 .. "! :)\nUse 'space-T' to switch between themes.", "info", { title = " 42-Nvim" })
+else
+	vim.notify("If you see this you haven't configured your stuff!\nDon't forget to take a peek at your ~/.config/nvim", "error", { title = " 42-Nvim" })
 end
