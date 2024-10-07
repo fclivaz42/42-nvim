fce34337ebf315a4ff545133e5f19e3f5d5cc613
fclivaz42 <fclivaz@pm.me>
2024-10-08 01:25:04 +0200

    dashboard-update

--- lua/config/builtin/_cmp.lua
+++ lua/config/builtin/_cmp.lua
@@ -21,7 +21,7 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.insert,
       select = false,
     },
-    ['<Down>'] = cmp.mapping(function(fallback)
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_locally_jumpable() then
@@ -30,15 +30,15 @@ cmp.setup {
         fallback()
       end
     end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
+    -- ['<S-Tab>'] = cmp.mapping(function(fallback)
+    --   if cmp.visible() then
+    --     cmp.select_prev_item()
+    --   elseif luasnip.locally_jumpable(-1) then
+    --     luasnip.jump(-1)
+    --   else
+    --     fallback()
+    --   end
+    -- end, { 'i', 's' }),
   },
   sources = {
     { name = 'nvim_lsp' },
