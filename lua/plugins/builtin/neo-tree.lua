53959b1af8e6284a272d3bdc958d1e74d1703ac2
fclivaz42 <fclivaz@pm.me>
2024-12-28 10:02:41 +0100

    dashboard-update

--- lua/plugins/builtin/neo-tree.lua
+++ lua/plugins/builtin/neo-tree.lua
@@ -11,14 +11,14 @@ return {
 			"MunifTanjim/nui.nvim",
 			lazy = true
 		},
-		-- {
-		-- 	"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-		-- 	build = false,
-		-- 	lazy = true,
-		-- 	opts = {
-		-- 		processor = "magick_cli"
-		-- 	}
-		-- },
+		{
+			"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+			build = false,
+			lazy = true,
+			opts = {
+				processor = "magick_cli"
+			}
+		},
 		{
 			"s1n7ax/nvim-window-picker",
 			event = 'VeryLazy',
